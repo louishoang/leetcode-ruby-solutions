28bb1957bc663de81d07d25eee5dfaec056dbb73
Spencer King <spencerking@users.noreply.github.com>
2017-05-20 19:35:11 -0500

    520 Detect Capital

---
+++ 520 Detect Capital/detectcapital.rb
@@ -0,0 +1,13 @@
+# @param {String} word
+# @return {Boolean}
+def detect_capital_use(word)
+    if word == word.upcase
+        return true
+    elsif word == word.downcase
+        return true
+    elsif word[0] == word[0].upcase and word[1..word.size] == word[1..word.size].downcase
+        return true
+    else
+        return false
+    end
+end
