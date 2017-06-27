61db61d455c78e4262284af8f474f4773fc2f6fd
Spencer King <spencerking@users.noreply.github.com>
2017-06-26 21:22:51 -0500

    21 Merge Two Sorted Lists

---
+++ 21 Merge Two Sorted Lists/merge_two_sorted_lists.rb
@@ -0,0 +1,27 @@
+# Definition for singly-linked list.
+# class ListNode
+#     attr_accessor :val, :next
+#     def initialize(val)
+#         @val = val
+#         @next = nil
+#     end
+# end
+
+# @param {ListNode} l1
+# @param {ListNode} l2
+# @return {ListNode}
+def merge_two_lists(l1, l2)
+    if !l1
+        return l2
+    elsif !l2
+        return l1
+    end
+    
+    if l1.val <l2.val
+        l1.next = merge_two_lists(l1.next, l2)
+        return l1
+    else
+        l2.next = merge_two_lists(l1, l2.next)
+        return l2
+    end
+end
